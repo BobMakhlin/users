924efa229d56dc5b277952814cd0338f8408dfb1
BobMakhlin <borismakhlin5@gmail.com>
2022-11-07 23:46:43 +0200

    Add Card component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Card from './components/UI/Card';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card>
+        <div>Hello</div>
+        <div>World</div>
+      </Card>
     </div>
   );
 }
