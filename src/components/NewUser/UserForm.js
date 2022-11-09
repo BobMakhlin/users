083958f3f718a796832505bd597897557a45f25a
BobMakhlin <borismakhlin5@gmail.com>
2022-11-09 23:13:16 +0200

    Add modal component

--- src/components/NewUser/UserForm.js
+++ src/components/NewUser/UserForm.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import styles from "./UserForm.module.css";
 import { v4 as uuidv4 } from "uuid";
 import Button from "../UI/Button";
+import Modal from "../UI/Modal";
 
 const UserForm = ({ onSubmit }) => {
   const [username, setUsername] = useState("");
@@ -25,27 +26,42 @@ const UserForm = ({ onSubmit }) => {
   };
 
   return (
-    <Card>
-      <form onSubmit={handleFormSubmit}>
-        <div className={styles.controls}>
-          <div className={styles.control}>
-            <label>Username</label>
-            <input
-              value={username}
-              onChange={handleUsernameChange}
-              type="text"
-            />
+    <>
+      <Modal isOpen={true}>
+        <div
+          style={{
+            position: "absolute",
+            width: "50rem",
+            height: "10rem",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            background: "white"
+          }}
+        ></div>
+      </Modal>
+      <Card>
+        <form onSubmit={handleFormSubmit}>
+          <div className={styles.controls}>
+            <div className={styles.control}>
+              <label>Username</label>
+              <input
+                value={username}
+                onChange={handleUsernameChange}
+                type="text"
+              />
+            </div>
+            <div className={styles.control}>
+              <label>Age (Years)</label>
+              <input value={age} onChange={handleAgeChange} type="number" />
+            </div>
           </div>
-          <div className={styles.control}>
-            <label>Age (Years)</label>
-            <input value={age} onChange={handleAgeChange} type="number" />
+          <div className={styles.actions}>
+            <Button>Add User</Button>
           </div>
-        </div>
-        <div className={styles.actions}>
-          <Button>Add User</Button>
-        </div>
-      </form>
-    </Card>
+        </form>
+      </Card>
+    </>
   );
 };
 
